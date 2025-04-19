5e22b85ad3dbc6c9c0bbe1a7cdb8848662516149
Ayana <149200009+hanashiroayana@users.noreply.github.com>
2025-04-19 14:54:11 -0300

    Add files via upload

---
+++ Jogo_Forca/forca.h
@@ -0,0 +1,10 @@
+#define TAMANHO_PALAVRA 20
+void abertura();
+void chuta();
+void desenhaforca();
+void escolhepalavra();
+void addpalavra();
+int acertou();
+int enforcado();
+int chutou(char letra);
+int chuteserrados();
